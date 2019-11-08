c9871226aee3f3084245e7cfd8bd031ce9b81366
Raghu Betina <raghu@firstdraft.com>
2019-11-08 10:21:44 -0600

    Order movies#index by newest

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all.order({ :year => :asc })
+    @movies = Movie.all.order({ :created_at => :desc })
     
     respond_to do |format|
       format.json do
